b071f58c2c1aba36276e78cc4a2bc56df66d1137
Jean Paul Ari Maldonado <jean.ari@ucsp.edu.pe>
2022-05-12 22:25:25 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Tick({ name }){
+    return(<div>
+        <h1>Hola {name}</h1>
+        <h2>Actualmente son las : {new Date().toLocaleTimeString()}.</h2>
+    </div>);
+}
+const root= createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Tick name="Usuario"/>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
