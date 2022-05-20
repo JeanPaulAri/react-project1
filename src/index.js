406fb21693bda6fc000ccc0f71d8e9f07c237aee
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-05-19 21:30:29 -0500

    modifiying files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
-import {createRoot} from "react-dom/client";
+import {createRoot} from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
 
-function Tick({ name }){
-    return(<div>
-        <h1>Hola {name}</h1>
-        <h2>Actualmente son las : {new Date().toLocaleTimeString()}.</h2>
-    </div>);
-}
 const root= createRoot(document.getElementById('root'));
 
-root.render(
-    <Tick name="Usuario"/>
-
-);
+root.render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
